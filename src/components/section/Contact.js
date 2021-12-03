ebe1e6e81394273fefe0986436c589fec86980da
Prakash <prakashchandraaec@gmail.com>
2021-12-03 20:04:35 +0530

    Added marging to button

--- src/components/section/Contact.js
+++ src/components/section/Contact.js
@@ -7,19 +7,19 @@ const Contact = () => {
         <div className="parallax">
                 <div data-aos="zoom-in-up" data-aos-once="true" className="git-form">
                     <>
-                <div className="git-head-div text-center mx-auto">
+                <div className="git-head-div text-center mx-auto" style={{marginTop:"-50px"}}>
                         <h1 id="Contact" className="git-head">{section5Title}</h1>
                 </div>
                 </>
                 <div className="container">
-            <div className="git-cont row">
+            <div className="git-cont row"  >
                 <div className="col-12 col-sm-6 half">
                     <form action={contact.contactUrl ? contact.contactUrl : "https://formspree.io"} method={contact.contactUrl ? "POST" : "GET"}>
                         <input type="text" id="fname" name="firstname" placeholder="Your name" required></input>
                         <input type="mail" id="mailid" name="Email" placeholder="Email Address" required></input>
                         <input type="text" id="sub" name="Subject" placeholder="Subject" required></input>
                         <textarea id="msg" name="message" placeholder="Message" required></textarea>
-                        <button style={{cursor: 'pointer'}} type="submit">Send Message</button>
+                        <button style={{cursor: 'pointer', marginTop:"-5px" }} type="submit">Send Message</button>
                     </form>
                 </div>
                 <div className="col-12 col-sm-6 half">
